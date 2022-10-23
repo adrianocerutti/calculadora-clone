40b1010db5a5ff033f6629d361d7be3f5aad8d41
Adriano B. Cerutti <adrianocerutti@gmail.com>
2022-10-23 16:13:33 -0300

    Adicionada lógica do botão Ponto e correção de bugs com o Ponto

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -31,6 +31,8 @@ class CalcController {
 
   clearAll() {
     this._operation = [];
+    this._lastNumber = "";
+    this._lastOperator = "";
 
     this.setLastNumberToDisplay();
   }
@@ -129,8 +131,6 @@ class CalcController {
     if (isNaN(this.getLastOperation())) {
       if (this.isOperator(value)) {
         this.setLastOperation(value);
-      } else if (isNaN(value)) {
-        console.log("outra coisa", value);
       } else {
         this.pushOperation(value);
 
@@ -142,7 +142,7 @@ class CalcController {
       } else {
         let newValue = this.getLastOperation().toString() + value.toString();
 
-        this.setLastOperation(parseInt(newValue));
+        this.setLastOperation(newValue);
 
         this.setLastNumberToDisplay();
       }
@@ -151,6 +151,22 @@ class CalcController {
 
   setError() {
     this.displayCalc = "Error";
+  }
+
+  addDot() {
+    let lastOperation = this.getLastOperation();
+
+    if (
+      typeof lastOperation === "string" &&
+      lastOperation.split("").indexOf(".") > -1
+    )
+      return;
+    if (this.isOperator(lastOperation) || !lastOperation) {
+      this.pushOperation("0.");
+    } else {
+      this.setLastOperation(lastOperation.toString() + ".");
+    }
+    this.setLastNumberToDisplay();
   }
 
   execBtn(value) {
@@ -188,7 +204,7 @@ class CalcController {
         break;
 
       case "ponto":
-        this.addOperation(".");
+        this.addDot();
         break;
 
       case "0":
