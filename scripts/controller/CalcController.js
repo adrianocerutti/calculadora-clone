8aa17fe7591627bfce918e337156c6c477fbc3dc
Adriano Cerutti <adrianocerutti@gmail.com>
2022-10-25 10:40:05 -0300

    Trabalhando com áudio

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,5 +1,7 @@
 class CalcController {
 	constructor() {
+		this._audio = new Audio("click.mp3");
+		this._audioOnOff = false;
 		this._lastOperator = "";
 		this._lastNumber = "";
 
@@ -45,10 +47,28 @@ class CalcController {
 
 		this.setLastNumberToDisplay();
 		this.pasteFromClipboard();
+
+		document.querySelectorAll(".btn-ac").forEach((btn) => {
+			btn.addEventListener("dblclick", (e) => {
+				this.toggleAudio();
+			});
+		});
+	}
+
+	toggleAudio() {
+		this._audioOnOff = !this._audioOnOff;
+	}
+
+	playAudio() {
+		if (this._audioOnOff) {
+			this._audio.currentTime = 0;
+			this._audio.play();
+		}
 	}
 
 	initKeyboard() {
 		document.addEventListener("keyup", (e) => {
+			this.playAudio();
 			switch (e.key) {
 				case "Escape":
 					this.clearAll();
@@ -251,6 +271,7 @@ class CalcController {
 	}
 
 	execBtn(value) {
+		this.playAudio();
 		switch (value) {
 			case "ac":
 				this.clearAll();
